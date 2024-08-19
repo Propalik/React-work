c213bd0c48bfed212bd0c60e6ff6649736c30915
Андрей Петров <andrey_petrov_2017@mail.ru>
2024-08-19 19:41:31 +0300

    №2
    создание карточек, добавление тайлваинд, попытка разобраться с непредвиденной ошибкой

--- src/components/ui/Card/Card.jsx
+++ src/components/ui/Card/Card.jsx
@@ -1,20 +1,35 @@
-import styles from "./Card.module.css";
-
 /**
  * Компонент карточка
- * @property{props.name} - Название карточки
- * @property{props.description} - Описание карточки
- * @returrns {JSX} Элемент JSX
+ * @property {string} props.title - Название карточки
+ * @property {string} props.category - Категория карточки
+ * @property {string} props.description - Описание карточки
+ * @property {string} props.price - Цена карточки
+ * @property {number} props.rating - Рейтинг карточки
+ * @property {string} props.imgSrc - Путь к изображению
+ * @returns {JSX.Element} Элемент JSX
  */
-export const Card = ({ title, subtitle, image }) => {
+export const Card = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { title, category, description, price, imgSrc } = props.details;
+
   return (
-    <div className={styles.card}>
-      <div className="card_image ">
-        <img src={image} alt={title} />
+    <div className="max-w-80 rounded-md overflow-hidden shadow-md hover:shadow-lg mb-4">
+      <div className="relative">
+        <img className="w-full max-h-44" src={imgSrc} alt={title} />
+        <div className="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">
+          SALE
+        </div>
       </div>
-      <div className={styles.cardImage}>
-        <h3 className={styles.card_title}>{title}</h3>
-        <p className={styles.card_subtitle}>{subtitle}</p>
+      <div className="p-4">
+        <h3 className="text-lg font-medium mb-2">{title}</h3>
+        <p className="text-gray-600 text-sm mb-4">{description}</p>
+        <p className="text-gray-600 text-sm mb-4">{category}</p>
+        <div className="flex items-center justify-between">
+          <span className="font-bold text-lg">{price}</span>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            Buy Now
+          </button>
+        </div>
       </div>
     </div>
   );
